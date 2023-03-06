74c1515a677039261231b435a5422d9fd103eb30
Camilo Ibanez <camiloibanez8@gmail.com>
2023-03-06 17:08:30 -0600

    react state and events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,8 +2,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    
+function ExpenseItem(props) {  
     return (
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -5,13 +7,21 @@ import './Expenses.css';
 function Expenses(props) {
     const items = props.expenses;
 
+    const [filterYear, setFilterYear] = useState('2022');
+    
+    const filterByYearHandler = (year) => {
+       setFilterYear(year);
+       console.log(year); 
+    };
+    
     return (
-        <Card className="expenses">
-            <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
-            <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
-            <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-            <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
-        </Card>
+            <Card className="expenses">
+                <ExpensesFilter selectedYear={filterYear} onFilterByYear={filterByYearHandler}/>
+                <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
+                <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
+                <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
+                <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
+            </Card>
     );
 }
 
